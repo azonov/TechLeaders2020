c909a6e29dea15f3125257f804e8ab551220a438
Vonckad <Vonkad@yandex.ru>
2020-10-14 23:25:58 +0300

    Создал структуру Items, и добавил в массив item 10 записей для примера.

---
+++ MessageBoard/MessageBoard/Model.swift
@@ -0,0 +1,15 @@
+//
+//  Model.swift
+//  MessageBoard
+//
+//  Created by Vlad Ralovich on 10/14/20.
+//
+
+import Foundation
+import UIKit
+
+struct Items {
+    var title: String
+    var info: String
+    var photos: Array<UIImage>
+}
