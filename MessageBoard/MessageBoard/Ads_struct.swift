296fac4673aa4d76fa48e575ed52f8c97e897d2d
User <user@macbook-air-user.local>
2020-10-19 23:32:40 +0300

    создал файл Ads_struct

---
+++ MessageBoard/MessageBoard/Ads_struct.swift
@@ -0,0 +1,22 @@
+//
+//  Ads_struct.swift
+//  MessageBoard
+//
+//  Created by User on 19.10.2020.
+//
+
+
+import Foundation
+import UIKit
+
+struct Ad_Struct {
+    let adCategory: String
+    var adName: String
+    var adTitle: String
+    var adAdress: String
+    var adPrice: Int
+}
+
+struct Ad_Photos {
+    var adPhotos: [UIImage]
+}
