8d43ed59b3102e0729c15690c9cade356ff0bc4e
User <user@macbook-air-user.local>
2020-11-03 17:42:13 +0300

    created Ad_list_screen.swift and Ad_list_screen_cell.swift

---
+++ MessageBoard/MessageBoard/Ad_list_screen.swift
@@ -0,0 +1,17 @@
+//
+//  Ad_list_screen.swift
+//  MessageBoard
+//
+//  Created by User on 03.11.2020.
+//
+
+import UIKit
+
+class Ad_list_screen: UITableViewController {
+
+    convenience init(ObjectID: MyItem) {
+        self.init()
+    }
+    
+
+}
