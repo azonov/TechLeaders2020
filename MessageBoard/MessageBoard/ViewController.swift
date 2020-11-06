591b71c0c49316c54a09de52e7440ee920d5fa51
Vonckad <Vonkad@yandex.ru>
2020-11-06 22:25:53 +0300

    Сделал экран с деталями объявления

--- MessageBoard/MessageBoard/ViewController.swift
+++ MessageBoard/MessageBoard/ViewController.swift
@@ -9,8 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet var priceLabel: UILabel!
+    @IBOutlet var adressLabel: UILabel!
+    @IBOutlet var titleNameLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    
+    @IBOutlet var aboutTextView: UITextView!
+    
+    @IBOutlet var callButton: UIButton!
+    
+    @IBOutlet var writeButton: UIButton!
+    
+    @IBOutlet var photoView: UIView!
+    
+    @IBOutlet var photoPageControl: UIPageControl!
+    
     override func viewDidLoad() {
-       
+      
+        callButton.layer.cornerRadius = 10
+        writeButton.layer.cornerRadius = 10
+        
     }
+    
+   
+    @IBAction func callButtonAction(_ sender: Any) {
+    }
+    @IBAction func writeButtonAction(_ sender: Any) {
+    }
+ 
 }
 
