b4313301706d1ec2212172a76cc4178f315fdde5
safonpa <safonov_p_a@rambler.ru>
2020-11-12 22:41:01 +0300

    Added UserID folder and StoryBoard view with UITable, UITableCell

---
+++ MessageBoard/MessageBoard/UserID/UserIDTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  UserIDCell.swift
+//  MessageBoard
+//
+//  Created by Admin on 12.11.2020.
+//
+
+import UIKit
+
+class UserIDTableViewCell : UITableViewCell {
+    
+    
+}
+
+
---
+++ MessageBoard/MessageBoard/UserID/UserIDViewController.swift
@@ -0,0 +1,13 @@
+//
+//  UserIDViewController.swift
+//  MessageBoard
+//
+//  Created by Admin on 12.11.2020.
+//
+
+import UIKit
+
+class UserIDViewController: UIViewController {
+    
+    
+}
